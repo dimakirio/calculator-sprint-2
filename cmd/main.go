d2213a99d98c04983a4bda5095902f9cea1911b5
dimakirio <kirionenkodima@gmail.com>
2025-02-25 17:14:47 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"calc_service/internal/agent"
+	"calc_service/internal/handlers"
+	"calc_service/internal/orchestrator"
+)
+
+func main() {
+	orchestratorInstance := orchestrator.NewOrchestrator()
+	orchestratorInstance.Start()
+
+	agent := agent.NewAgent(orchestratorInstance, 1)
+	go agent.Start()
+
+	http.HandleFunc("/api/v1/calculate", handlers.CalculateHandler)
+	http.HandleFunc("/api/v1/expressions", handlers.ExpressionsHandler)
+	http.HandleFunc("/api/v1/expressions/", handlers.ExpressionByIDHandler)
+
+	log.Println("Starting calculator service on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Could not start server: %v", err)
+	}
+}
