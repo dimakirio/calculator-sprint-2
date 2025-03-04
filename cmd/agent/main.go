c716d2d8056c33d11abd7d2f845940308fc75067
dimakirio <kirionenkodima@gmail.com>
2025-03-04 20:45:42 +0500

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,1 +1,13 @@
+package main
 
+import (
+	"log"
+
+	"github.com/dimakirio/calculator-sprint-2/internal/application"
+)
+
+func main() {
+	agent := application.NewAgent()
+	log.Println("Starting Agent...")
+	agent.Run()
+}
